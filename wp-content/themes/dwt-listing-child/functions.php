3ba74b75b81cbc279ca7f9edee71c3d777b0668d
kuzmich84 <kudim@inbox.ru>
2021-06-08 06:32:46 +0300

    Создает шорткод WPBakery

--- wp-content/themes/dwt-listing-child/functions.php
+++ wp-content/themes/dwt-listing-child/functions.php
@@ -1,24 +1,35 @@
 <?php
 
-add_action( 'after_setup_theme', 'my_child_theme_setup' );
-function my_child_theme_setup(){
-    load_child_theme_textdomain( 'dwt-listing', get_stylesheet_directory() . '/languages' );
+add_action('after_setup_theme', 'my_child_theme_setup');
+function my_child_theme_setup()
+{
+    load_child_theme_textdomain('dwt-listing', get_stylesheet_directory() . '/languages');
 
-    function theme_register_nav_menu() {
-        register_nav_menu( 'primary', 'Primary Menu' );
+    function theme_register_nav_menu()
+    {
+        register_nav_menu('primary', 'Primary Menu');
     }
 }
 
-add_action( 'wp_enqueue_scripts', 'dwt_listing_child_scripts' );
-function dwt_listing_child_scripts(){
-    wp_enqueue_style( 'style-suggestions', 'https://cdn.jsdelivr.net/npm/suggestions-jquery@20.3.0/dist/css/suggestions.min.css');
-    wp_enqueue_style('parent-theme-css', get_template_directory_uri() .'/style.css' );
-    wp_enqueue_style('udemy-css', get_stylesheet_directory_uri().'/assets/css/style.css');
+add_action('wp_enqueue_scripts', 'dwt_listing_child_scripts');
+function dwt_listing_child_scripts()
+{
+    wp_enqueue_style('style-suggestions', 'https://cdn.jsdelivr.net/npm/suggestions-jquery@20.3.0/dist/css/suggestions.min.css');
+    wp_enqueue_style('parent-theme-css', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('udemy-css', get_stylesheet_directory_uri() . '/assets/css/style.css');
+    wp_enqueue_style('udemy-slider-css', get_stylesheet_directory_uri() . '/assets/css/slider.css');
 
-    wp_enqueue_script( 'jquery-ajax--script', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js', '', '', true);
-    wp_enqueue_script( 'suggestions-jquery', 'https://cdn.jsdelivr.net/npm/suggestions-jquery@20.3.0/dist/js/jquery.suggestions.min.js','', '', true);
-    wp_enqueue_script( 'dadata-script', get_stylesheet_directory_uri().'/scripts/dadata.js', '', '', true);
-    wp_enqueue_script( 'my-custom-script', get_stylesheet_directory_uri().'/scripts/my-custom.js', '', '', true);
+    wp_enqueue_script('jquery-ajax--script', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js', '', '', true);
+    wp_enqueue_script('suggestions-jquery', 'https://cdn.jsdelivr.net/npm/suggestions-jquery@20.3.0/dist/js/jquery.suggestions.min.js', '', '', true);
+    wp_enqueue_script('dadata-script', get_stylesheet_directory_uri() . '/scripts/dadata.js', '', '', true);
+    wp_enqueue_script('my-custom-script', get_stylesheet_directory_uri() . '/scripts/my-custom.js', '', '', true);
+
+
+    wp_enqueue_script('edumy-slider-script', get_stylesheet_directory_uri() . '/assets/js/slider.js', '', '', true);
+    wp_enqueue_script('bootstrap-script', get_stylesheet_directory_uri() . '/assets/js/bootstrap.min.js', '', '', true);
+    wp_enqueue_script('bootstrap-select-script', get_stylesheet_directory_uri() . '/assets/js/bootstrap-select.min.js', '', '', true);
+    wp_enqueue_script('edumy-script', get_stylesheet_directory_uri() . '/assets/js/script.js', '', '', true);
+
 
 
     /**
@@ -32,11 +43,11 @@ function dwt_listing_child_scripts(){
         if (isset($dwt_listing_options["dwt_listing_yandex_lang"]) && $dwt_listing_options["dwt_listing_yandex_lang"] != "") {
             $map_lang = $dwt_listing_options["dwt_listing_yandex_lang"];
         }
-        wp_enqueue_script("yandex-map", "https://api-maps.yandex.ru/2.1/?apikey=". $dwt_listing_options["yandex_api_key"]."&lang=". $map_lang , false, false, false);
+        wp_enqueue_script("yandex-map", "https://api-maps.yandex.ru/2.1/?apikey=" . $dwt_listing_options["yandex_api_key"] . "&lang=" . $map_lang, false, false, false);
         //call back
 //        wp_register_script("google-map-callback", "//maps.googleapis.com/maps/api/js?key=" . $dwt_listing_options["gmap_api_key"] . "&libraries=geometry,places&language=" . $map_lang . "&callback=" . "dwt_listing_location", false, false, true);
     }
-    wp_enqueue_script("getYandexMap", get_stylesheet_directory_uri().'/scripts/yandex-map.js', false, false, false);
+    wp_enqueue_script("getYandexMap", get_stylesheet_directory_uri() . '/scripts/yandex-map.js', false, false, false);
 
 }
 
@@ -44,13 +55,14 @@ function dwt_listing_child_scripts(){
  * Функция проверяющая роль пользователя
  */
 
-function is_user_role( $role, $user_id = null ) {
-    $user = is_numeric( $user_id ) ? get_userdata( $user_id ) : wp_get_current_user();
+function is_user_role($role, $user_id = null)
+{
+    $user = is_numeric($user_id) ? get_userdata($user_id) : wp_get_current_user();
 
-    if( ! $user )
+    if (!$user)
         return false;
 
-    return in_array( $role, (array) $user->roles );
+    return in_array($role, (array)$user->roles);
 }
 
 
@@ -61,20 +73,22 @@ function is_user_role( $role, $user_id = null ) {
 //
 // Удаляем роль при деактивации нашей темы
 
-add_action( 'switch_theme', 'deactivate_dwt_listing_child' );
-function deactivate_dwt_listing_child() {
-    remove_role( 'student' );
+add_action('switch_theme', 'deactivate_dwt_listing_child');
+function deactivate_dwt_listing_child()
+{
+    remove_role('student');
 }
 
 // Добавляем роль при активации нашей темы
-add_action( 'after_switch_theme', 'activate_dwt_listing_child' );
-function activate_dwt_listing_child() {
+add_action('after_switch_theme', 'activate_dwt_listing_child');
+function activate_dwt_listing_child()
+{
 
 
-    add_role( 'student', 'Студент', array(
-        'read'            => true, // Allows a user to read
-        'create_posts'      => true, // Allows user to create new posts
-        'edit_posts'        => true, // Allows user to edit their own posts
+    add_role('student', 'Студент', array(
+        'read' => true, // Allows a user to read
+        'create_posts' => true, // Allows user to create new posts
+        'edit_posts' => true, // Allows user to edit their own posts
         'edit_others_posts' => false, // Allows user to edit others posts too
         'publish_posts' => true, // Allows the user to publish posts
         'manage_categories' => true, // Allows user to manage post categories
@@ -83,6 +97,56 @@ function activate_dwt_listing_child() {
 
 
 /**
- * Класс для кастомизации вывода категорий
+ * Создание своего виджета в WpBackery
  */
 
+if (class_exists("WPBakeryShortCode")) {
+    // Class Name should be WPBakeryShortCode_Your_Short_Code
+    // See more in vc_composer/includes/classes/shortcodes/shortcodes.php
+    class WPBakeryShortCode_My_Shortcodes extends WPBakeryShortCode
+    {
+    }
+}
+
+if (function_exists('vc_map')) {
+    vc_map(array(
+            'name' => __('Edumy Home Join', 'edumy'),
+            // shortcode name
+
+            'base' => 'my_shortcodes',
+            // shortcode base [test_element]
+
+            'category' => __('Edumy Home', 'edumy'),
+            // param category tab in add elements view
+
+            'description' => __('Create join section on home page', 'edumy'),
+            // element description in add elements view
+
+            'show_settings_on_create' => true,
+            // don't show params window after adding
+
+            'icon'=> 'icon-vc-edumy',
+
+
+            'weight' => -5,
+            // Depends on ordering in list, Higher weight first
+
+            'front_enqueue_js' => get_stylesheet_directory_uri().'/assets/js/slider.js',
+
+            'params' => array(
+                array(
+                    "type" => "textfield",
+                    "heading" => __("Chart value(1-100)", "edumy"),
+                    "param_name" => "value",
+                    "description" => __("Chart value(number).", "edumy")
+                ),
+                array(
+                    "type" => "attach_image",
+                    "heading" => __("Photo Join", "edumy"),
+                    "param_name" => "join_photo",
+                    "description" => __("Upload photo", "edumy")
+                ),
+            )
+        )
+    );
+}
---
+++ wp-content/themes/dwt-listing-child/vc_templates/my_shortcodes.php
@@ -0,0 +1,45 @@
+<section class="home-five bg-img5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="home5_slider">
+                    <div class="item">
+                        <div class="home-text">
+                            <h2>We Can T<span class="text-thm">each You!</span></h2>
+                            <p>Technology is brining a massive wave of evolution on learning things on different ways.</p>
+                            <a class="btn home_btn" href="#">Join In Free</a>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="home-text">
+                            <h2>We Can T<span class="text-thm">each You!</span></h2>
+                            <p>Technology is brining a massive wave of evolution on learning things on different ways.</p>
+                            <a class="btn home_btn" href="#">Join In Free</a>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="home-text">
+                            <h2>We Can T<span class="text-thm">each You!</span></h2>
+                            <p>Technology is brining a massive wave of evolution on learning things on different ways.</p>
+                            <a class="btn home_btn" href="#">Join In Free</a>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="home-text">
+                            <h2>We Can T<span class="text-thm">each You!</span></h2>
+                            <p>Technology is brining a massive wave of evolution on learning things on different ways.</p>
+                            <a class="btn home_btn" href="#">Join In Free</a>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="home-text">
+                            <h2>We Can T<span class="text-thm">each You!</span></h2>
+                            <p>Technology is brining a massive wave of evolution on learning things on different ways.</p>
+                            <a class="btn home_btn" href="#">Join In Free</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
